e226ff37374f8c3422584b519c16f8d55afbc7fd
Wilton Oliveira <oliveirajrwilton@gmail.com>
2018-07-24 14:42:29 -0300

    Adicionado Formilário de contato

---
+++ js/formulario.js
@@ -0,0 +1,9 @@
+function enviar() {
+  var informacoes = document.getElementById("nome");
+
+  if (nome.value != ""){
+    alert("Obrigado " + nome.value +"! Mas está área ira se auto destruir em breve");
+  } else {
+    alert("Por gentileza, coloque seus dados");
+  }
+}
